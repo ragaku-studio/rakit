23ca61b6666991adc9034f13aa24920e0f729c7c
Radhitia <radhitia.dwijaya@gmail.com>
2026-09-14 11:54:28 +0700

    Update CSS and Create Folder Docs

---
+++ docs/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+  <title>Ragaku UI Kit</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700&family=DM+Sans:wght@400;500&display=swap"
+    rel="stylesheet" />
+  <link rel="stylesheet" href="rakit.css" />
+  <link rel="stylesheet" href="tonggol.css" />
+  <script src="rk.js"></script>
+</head>
+
+<body>
